f497c5f75ee18eb88838bd9f901bd2d306b13bf1
EhWanu <cameronjshadgett@gmail.com>
2022-09-08 13:07:29 -0400

    animation and basic about me typography

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,43 @@
-import React from "react";
-
+import "./intro.css";
 const Intro = () => {
-	return <div>Intro</div>;
+	return (
+		<div className="i">
+			<div className="i-left">
+				<div className="i-left-wrapper">
+					<h2 className="i-intro">
+						Hello, My name is
+					</h2>
+					<h1 className="i-name ">Cam</h1>
+					<div className="i-title">
+						<div className="i-title-wrapper">
+							<div className="i-title-item">
+								Web Developer
+							</div>
+							<div className="i-title-item">
+								UI Design
+							</div>
+							<div className="i-title-item">
+								Frontend Developer
+							</div>
+							<div className="i-title-item">
+								Backend Developer
+							</div>
+							<div className="i-title-item">
+								Content Creator
+							</div>
+						</div>
+					</div>
+					<div className="i-desc">
+						I design and develop services for customers
+						of all, specializing in creating stylish,
+						modern websites, web services and online
+						stores.
+					</div>
+				</div>
+			</div>
+			<div className="i-right">right</div>
+		</div>
+	);
 };
 
 export default Intro;
