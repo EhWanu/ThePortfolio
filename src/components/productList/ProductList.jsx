2980796f6b2cc50864682009c033137d81924a3c
EhWanu <cameronjshadgett@gmail.com>
2022-09-08 16:10:29 -0400

    base layout for portfolio list

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -2,7 +2,24 @@ import Product from "../product/Product";
 import "./productList.css";
 
 const ProductList = () => {
-	return <div></div>;
+	return (
+		<div className="pl">
+			<div className="pl-texts">
+				<h1 className="pl-title">Create & inspire</h1>
+				<p className="pl-desc">
+					Cutting edge developments for your business
+				</p>
+			</div>
+			<div className="pl-list">
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+			</div>
+		</div>
+	);
 };
 
 export default ProductList;
