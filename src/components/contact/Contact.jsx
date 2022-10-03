2f57b97682fb4f3f68b3d7f9dc07b9e07f04bac4
EhWanu <cameronjshadgett@gmail.com>
2022-10-03 12:35:40 -0400

    created right and left flex divs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,18 @@
 import "./contact.css";
 
 const Contact = () => {
-	return <div></div>;
+	return (
+		<div className="c">
+			<div className="c-bg"></div>
+			{/* Wrapper that contains a right and left div */}
+			<div className="c-wrapper">
+				{/* Left div */}
+				<div className="c-left"></div>
+				{/* Right div */}
+				<div className="c-right">vd</div>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
